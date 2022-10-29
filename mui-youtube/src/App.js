4075c356a7d09f73c7752b91185d19930414c038
Benazir Sultana <benazirsultana1998@gmail.com>
2022-10-29 21:41:06 +0530

    components and routes added

--- mui-youtube/src/App.js
+++ mui-youtube/src/App.js
@@ -1,10 +1,25 @@
-
-
+import React from 'react'
+import { BrowserRouter, Routes,Route } from 'react-router-dom'
+import {Box} from "@mui/material"
+import Feed from './Feed';
+import VideoDetails from './VideoDetails';
+import ChannelDetail from './ChannelDetail';
+import SearchFeed from './SearchFeed';
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <BrowserRouter>
+    <Box sx={{ backgroungColor:"#000"}}>
+<Navbar/>
+<Routes>
+
+  <Route path='/' exact element={<Feed/>} />
+  <Route path='/video/:id' exact element={<VideoDetails/>} />
+  <Route path='/channel/:id' exact element={<ChannelDetail/>} />
+  <Route path='/search/:id' exact element={<SearchFeed/>} />
+</Routes>
+
+    </Box>
+    </BrowserRouter>
   );
 }
 
