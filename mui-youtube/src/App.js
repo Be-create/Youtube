f4aefeb32de0ec7a865890139941d8684a2fbe04
Benazir Sultana <benazirsultana1998@gmail.com>
2023-02-12 19:35:26 +0530

    Starting Fresh

--- mui-youtube/src/App.js
+++ mui-youtube/src/App.js
@@ -2,28 +2,10 @@ import React from 'react'
 
 import { BrowserRouter, Routes,Route } from 'react-router-dom'
 import {Box} from "@mui/material"
-import {Navbar,Feed,SearchFeed,VideoDetails,ChannelDetail, Sidebar} from './components';
 
 function App() {
   return (
-    <BrowserRouter>
-    <Box>
-    <Navbar/>
-<Box  sx={{ display: 'flex' }}>
-  
-  <Sidebar/>
-<Routes>
-
-<Route path='/' exact element={<Feed/>} />
-<Route path='/video/:id' exact element={<VideoDetails/>} />
-<Route path='/channel/:id' exact element={<ChannelDetail/>} />
-<Route path='/search/:id' exact element={<SearchFeed/>} />
-</Routes>
-</Box>
-
-
-    </Box>
-    </BrowserRouter>
+    <Box>Youtube</Box>
   );
 }
 
--- mui-youtube/src/components/index.js
+++
@@ -1,6 +0,0 @@
-export {default as Navbar} from "./Navbar"
-export {default as Feed} from "./Feed"
-export {default as ChannelDetail} from "./ChannelDetail"
-export {default as SearchFeed} from "./SearchFeed"
-export {default as VideoDetails} from "./VideoDetails"
-export {default as Sidebar} from "./Sidebar"
