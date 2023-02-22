3bfc080ff1f9b9a87fd2a08b4e9fcc78c2aa7134
Benazir Sultana <benazirsultana1998@gmail.com>
2023-02-22 23:25:04 +0530

    add. icons fixed

--- mui-youtube/src/App.js
+++ mui-youtube/src/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 import { BrowserRouter, Routes,Route } from 'react-router-dom'
 import {Box} from "@mui/material"
-import Navbar from './components/navbar';
+import Navbar from './pages/components/navbar';
 
 function App() {
   return (
